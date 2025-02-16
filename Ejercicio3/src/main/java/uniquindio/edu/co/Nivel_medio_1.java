d528a5c60ce4b7cb3bc58e4c9cff490bb77f62b8
JuanMMS <juanm.mosqueras@uqvirtual.edu.co>
2025-02-16 16:16:05 -0500

    Update
    Se agrega ejercicio nivel medio, y el nivel alto de los ejercicios

--- Ejercicio3/src/main/java/uniquindio/edu/co/Nivel_medio_1.java
+++ Ejercicio3/src/main/java/uniquindio/edu/co/Nivel_medio_1.java
@@ -1,5 +1,32 @@
 package uniquindio.edu.co;
 
+import java.util.Scanner;
+
 public class Nivel_medio_1 {
-    
-}
+    public static void main(String[] args) {
+        int numero_n = ingresar_entero();
+        String triangulo = generar_triangulo(numero_n);
+        System.out.println(triangulo);
+    }
+
+    public static int ingresar_entero(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el número N para formar el triangulo: ");
+        int numero_n = Integer.parseInt(scanner.nextLine());
+        return numero_n;
+    }
+    public static String generar_triangulo(int numero_n){
+        String triangulo = "";
+        char forma = '#';
+        int i = 1;
+        while(i<=numero_n){
+            for(int j=i; j<=numero_n; j++){
+                triangulo += forma;
+            }
+            triangulo += "\n";
+            i++;
+        }
+        return triangulo;
+        }
+
+    }
