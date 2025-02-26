9e38b672c01f837512a9d0984c69352b43d3fbf5
JuanMMS <juanm.mosqueras@uqvirtual.edu.co>
2025-02-26 12:40:43 -0500

    Create Guarderia.java

---
+++ Activiadad_guarderia_RQ2/src/main/java/uniquindio/edu/co/Guarderia.java
@@ -0,0 +1,19 @@
+package uniquindio.edu.co;
+
+import java.awt.*;
+
+public class Guarderia {
+    private String nombre;
+    private List<Ninio> listNinios;
+
+    public Guarderia(String nombre){
+        this.nombre = nombre;
+        this.listNinios = new ArrayList<Ninio>();
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){}
+
+    public almacerna(Ninio ninio)
+}
